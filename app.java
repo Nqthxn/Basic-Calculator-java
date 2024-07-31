b6c3e428055bb20906fcd76dd50cfa8f9d0ba0c4
Nathan Lam <nathanl1605@gmail.com>
2024-07-31 13:43:04 +0100

    Added 'calculate' function to perform the arithmetaic operations

--- app.java
+++ app.java
@@ -3,14 +3,48 @@ import java.util.Scanner;
 class app{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int num1, num2;
+        float num1, num2;
+        char operator;
 
         System.out.println("----------[ CALCULATOR ]----------");
 
         System.out.print("Enter First Number : ");
-        num1 = input.nextInt();
+        num1 = input.nextFloat();
+
+        System.out.print("Enter Operator (+-*/) : ");
+        operator = input.next().charAt(0);
 
         System.out.print("Enter Second Number : ");
-        num2 = input.nextInt();
+        num2 = input.nextFloat();
+
+        float total = calculate(operator, num1, num2);
+
+        if(total != -1){
+            System.out.println("Result : " + String.format("%.2f", total));
+        }else{
+            System.out.println("Try Again");
+        }
+    }
+    public static float calculate(char op, float n1, float n2){
+        float result = 0;
+
+        switch(op){
+            case '+':
+                result = n1 + n2;
+                break;
+            case '-':
+                result = n1 - n2;
+                break;
+            case '*':
+                result = n1 * n2;
+                break;
+            case '/':
+                result = n1 / n2;
+                break;
+            default:
+                System.out.println("Invalid Operator");
+                return -1;
+        }
+        return result;
     }
 }
