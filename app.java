ea069915e07335e6bc880ae60de9edd6ce7ffd06
Nathan Lam <nathanl1605@gmail.com>
2024-07-31 13:30:09 +0100

    Added user input functionality and storing them in the correct variables

--- app.java
+++ app.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 class app{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        
+        int num1, num2;
+
+        System.out.println("----------[ CALCULATOR ]----------");
+
+        System.out.print("Enter First Number : ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter Second Number : ");
+        num2 = input.nextInt();
     }
 }
